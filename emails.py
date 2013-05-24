ec38bcd49ae49ca8ee219639e0a067601c3064de
Lee Jooyoung <petabytekr@gmail.com>
2013-05-24 18:47:09 +0900

    rename to gcentask

--- emails.py
+++ emails.py
@@ -10,8 +10,8 @@ from dateutil import *
 from model import *
 
 REMINDER = """
-한 주간 수고하셨습니다.
-회신으로 업무내역을 입력해주세요. :)
+수고하셨습니다.
+회신으로 업무내역을 입력해주세요. ;)
 
 지난주 한 일
 - 
@@ -30,7 +30,7 @@ class ReminderEmail(webapp.RequestHandler):
 
 class OneReminderEmail(webapp.RequestHandler):
     def post(self):
-        mail.send_mail(sender="snippets <snippets@minisnippets.appspotmail.com>", # FIX ME
+        mail.send_mail(sender="snippets <snippets@gcentask.appspotmail.com>", # FIX ME
                        to=self.request.get('email'),
                        subject="Snippet 타임!",
                        body=REMINDER)
@@ -47,7 +47,7 @@ class DigestEmail(webapp.RequestHandler):
 
 class OneDigestEmail(webapp.RequestHandler):
     def __send_mail(self, recipient, body):
-        mail.send_mail(sender="snippets <snippets@minisnippets.appspotmail.com>", # FIX ME
+        mail.send_mail(sender="snippets <snippets@gcentask.appspotmail.com>", # FIX ME
                        to=recipient,
                        subject="Snippet Digest 메일이 도착하였습니다!",
                        body=body)
@@ -68,6 +68,6 @@ class OneDigestEmail(webapp.RequestHandler):
         logging.info(all_snippets)
         body = '\n\n\n'.join([self.__snippet_to_text(s) for s in all_snippets if s.user.email in following])
         if body:
-            self.__send_mail(user.email, 'https://minisnippets.appspot.com\n\n' + body) # FIX ME
+            self.__send_mail(user.email, 'https://gcentask.appspot.com\n\n' + body) # FIX ME
         else:
             logging.info(user.email + ' not following anybody.')
